72bd25c671906078a40d5ecbc310977258373e4b
chris.hamons <chris.hamons@2fc39768-4c50-0410-98c4-473688cc0402>
2008-06-30 03:15:47 +0000

    Fixed bug in parser and finished auto test. Fixes issue 6.

--- TestHarness/EntryPoint.cs
+++ TestHarness/EntryPoint.cs
@@ -8,9 +8,9 @@ namespace TestHarness
         public static int Main()
         {
             //For C/C++ debug attachment
-            //TCODConsoleRoot console = new TCODConsoleRoot(80, 50, "Keyboard Tester", false);
-            //TCODKeyboard keyboard = new TCODKeyboard();
-            //TCOD_key key = keyboard.WaitForKeyPress(false);
+            TCODConsoleRoot console = new TCODConsoleRoot(80, 50, "Keyboard Tester", false);
+            TCODKeyboard keyboard = new TCODKeyboard();
+            TCOD_key key = keyboard.WaitForKeyPress(false);
             return TCODFileParserTest.TestFileParser() == true ? 1 : 0;
         }
     }
