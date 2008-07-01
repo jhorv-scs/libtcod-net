cf8aa336cc0290b083ec4ccbe6c7eb487e317e5a
chris.hamons <chris.hamons@2fc39768-4c50-0410-98c4-473688cc0402>
2008-07-01 03:01:31 +0000

    Fix keyboard manual test.

--- TestHarness/TestSuite/Manual Tests/TCODKeyboardTest.cs
+++ TestHarness/TestSuite/Manual Tests/TCODKeyboardTest.cs
@@ -9,12 +9,11 @@ namespace libtcodWrapperTests
         private static bool inRealTimeTest = false;
         public static bool TestTCODKeyboard()
         {
-
             bool testPassed = true;
             try
             {
-                CustomFontRequest font = new CustomFontRequest("Herrbdog_16x16_tileset.bmp", 16, 16, 16, 16, true, new TCODColor(255, 0, 255));
-                TCODConsoleRoot console = new TCODConsoleRoot(80, 50, "Keyboard Tester", false, font);
+                //CustomFontRequest font = new CustomFontRequest("Herrbdog_16x16_tileset.bmp", 16, 16, 16, 16, true, new TCODColor(255, 0, 255));
+                TCODConsoleRoot console = new TCODConsoleRoot(80, 50, "Keyboard Tester", false);
                 TCODKeyboard keyboard = new TCODKeyboard();
 
                 TCOD_key key = new TCOD_key();
