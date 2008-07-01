419d482bc6b4d78c987b6e72b72e34db0c5ca766
chris.hamons <chris.hamons@2fc39768-4c50-0410-98c4-473688cc0402>
2008-07-01 14:00:17 +0000

    Wrapping of TCODImage complete. Autotest in progress.

--- trunk/TestHarness/TestSuite/Manual Tests/ManualTestMain.cs
+++ trunk/TestHarness/TestSuite/Manual Tests/ManualTestMain.cs
@@ -8,10 +8,18 @@ namespace libtcodWrapperTests
 #if ManualTest
     public class ManualTest
     {
+        static void WaitForDebugger()
+        {
+            TCODConsoleRoot console = new TCODConsoleRoot(80, 50, "Keyboard Tester", false);
+            TCODKeyboard keyboard = new TCODKeyboard();
+            keyboard.WaitForKeyPress(false);
+        }
+
         static int Main()
         {
-            TCODFovTest.TestTCODFovTest();
-            TCODKeyboardTest.TestTCODKeyboard();
+            //TCODFovTest.TestTCODFovTest();
+            //TCODKeyboardTest.TestTCODKeyboard();
+            WaitForDebugger();
             return 0;
         }
     }
---
+++ trunk/TestHarness/TestSuite/TCODImageTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using libtcodWrapper;
+
+namespace libtcodWrapperTests
+{
+    [TestFixture]
+    public class TCODImageTest
+    {
+        private TCODConsoleRoot console;
+        [SetUp]
+        public void Init()
+        {
+            console = new TCODConsoleRoot(80, 50, "Image Testing", false);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            console.Dispose();
+        }
+        
+        [Test]
+        public void ImageConstructors()
+        {
+            TCODImage i = new TCODImage(40, 40);
+            TCODImage j = new TCODImage("terminal.bmp");
+            TCODImage k = new TCODImage(console);
+        }
+    }
+}
