6693183e88b8cf8c031de012991aa6b80fc690e2
chris.hamons <chris.hamons@2fc39768-4c50-0410-98c4-473688cc0402>
2008-07-13 00:05:03 +0000

    Update Windows Binaries for libtcod 1.3.2 mark III. Add some documentation for TCODColor and Doxyfile.

--- libtcodWrapper/TCODColor.cs
+++ libtcodWrapper/TCODColor.cs
@@ -4,11 +4,18 @@ using System.Text;
 
 namespace libtcodWrapper
 {
+    /// <summary>
+    /// Represents a 32-bit color to the TCOD API.
+    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
     public struct TCODColor
     {
         public byte r, g, b;
 
+        /// <summary>
+        /// Copy Constructor for TCODColor
+        /// </summary>
+        /// <param name="c">Color to make copy of.</param>
         public TCODColor(TCODColor c)
         {
             r = c.r;
@@ -16,13 +23,25 @@ namespace libtcodWrapper
             b = c.b;
         }
 
+        /// <summary>
+        /// Form a TCODColor from RGB components.
+        /// </summary>
+        /// <param name="red">Red Component (0 - 255)</param>
+        /// <param name="green">Green Component (0 - 255)</param>
+        /// <param name="blue">Blue Component (0 - 255)</param>
         public TCODColor(byte red, byte green, byte blue)
         {
             r = red;
             g = green;
             b = blue;
         }
-
+        
+        /// <summary>
+        /// Form a TCODColor from HSV components.
+        /// </summary>
+        /// <param name="h">Hue Component (0.0 - 360.0)</param>
+        /// <param name="s">Saturation Component (0.0 - 1.0)</param>
+        /// <param name="v">Value Component (0.0 - 1.0)</param>
         public TCODColor(float h, float s, float v)
         {
             r = 0;
@@ -32,11 +51,22 @@ namespace libtcodWrapper
             TCOD_color_set_HSV(ref this, h, s, v);
         }
 
+        /// <summary>
+        /// Returns HSV value from a TCODColor.
+        /// </summary>
+        /// <param name="h">Hue Component (0.0 - 360.0)</param>
+        /// <param name="s">Saturation Component (0.0 - 1.0)</param>
+        /// <param name="v">Value Component (0.0 - 1.0)</param>
         public void GetHSV(out float h, out float s, out float v)
         {
             TCOD_color_get_HSV(this, out h, out s, out v);
         }
 
+        /// <summary>
+        /// Determine if two TCODColors are equal.
+        /// </summary>
+        /// <param name="obj">Other TCODColor</param>
+        /// <returns>Are Equal?</returns>
         public override bool Equals(object obj)
         {
             if (obj == null || GetType() != obj.GetType())
@@ -45,41 +75,88 @@ namespace libtcodWrapper
             return TCOD_color_equals(this, (TCODColor)obj);
         }
 
+        /// <summary>
+        /// Calculate Hash Value of a TCODColor
+        /// </summary>
+        /// <returns>Hash Value</returns>
         public override int GetHashCode()
         {
             return base.GetHashCode();
         }
 
+        /// <summary>
+        /// Determine if two TCODColors are equal.
+        /// </summary>
+        /// <param name="lhs">Left Hand Size</param>
+        /// <param name="rhs">Right Hand Size</param>
+        /// <returns>Are Equal?</returns>
         public static bool operator ==(TCODColor lhs, TCODColor rhs)
         {
             return TCOD_color_equals(lhs, rhs);
         }
 
+        /// <summary>
+        /// Determine if two TCODColors are not equal.
+        /// </summary>
+        /// <param name="lhs">Left Hand Size</param>
+        /// <param name="rhs">Right Hand Size</param>
+        /// <returns>Are Not Equal?</returns>
         public static bool operator !=(TCODColor lhs, TCODColor rhs)
         {
             return !TCOD_color_equals(lhs, rhs);
         }
 
+        /// <summary>
+        /// Add two TCODColors
+        /// </summary>
+        /// <param name="lhs">Left Hand Size</param>
+        /// <param name="rhs">Right Hand Size</param>
+        /// <returns>New Color</returns>
         public static TCODColor operator +(TCODColor lhs, TCODColor rhs)
         {
             return TCOD_color_add(lhs, rhs);
         }
 
+        /// <summary>
+        /// Multiply two TCODColors
+        /// </summary>
+        /// <param name="lhs">Left Hand Size</param>
+        /// <param name="rhs">Right Hand Size</param>
+        /// <returns>New Color</returns>
         public static TCODColor operator *(TCODColor lhs, TCODColor rhs)
         {
             return TCOD_color_multiply(lhs, rhs);
         }
 
+        /// <summary>
+        /// Multiple a TCODColor by a constant
+        /// </summary>
+        /// <param name="lhs">Left Hand Size</param>
+        /// <param name="rhs">Right Hand Size</param>
+        /// <returns>New Color</returns>
         public static TCODColor operator *(TCODColor lhs, float rhs)
         {
             return TCOD_color_multiply_scalar(lhs, rhs);
         }
 
+        /// <summary>
+        /// Multiple a TCODColor by a constant
+        /// </summary>
+        /// <param name="lhs">Left Hand Size</param>
+        /// <param name="rhs">Right Hand Size</param>
+        /// <returns>New Color</returns>
         public static TCODColor operator *(TCODColor lhs, double rhs)
         {
             return TCOD_color_multiply_scalar(lhs, (float)rhs);
         }
 
+        /// <summary>
+        /// Interpolate (lerp) a TCODColor with another TCODColor
+        /// </summary>
+        /// <param name="c1">First Color</param>
+        /// <param name="c2">Second Color</param>
+        /// <param name="coef">Interpolate Coefficient</param>
+        /// <returns>New Color</returns>
         public static TCODColor Interpolate(TCODColor c1, TCODColor c2, float coef)
         {
         	TCODColor ret =  new TCODColor(); 
@@ -89,6 +166,13 @@ namespace libtcodWrapper
 			return ret;
         }
 
+        /// <summary>
+        /// Interpolate (lerp) a TCODColor with another TCODColor
+        /// </summary>
+        /// <param name="c1">First Color</param>
+        /// <param name="c2">Second Color</param>
+        /// <param name="coef">Interpolate Coefficient</param>
+        /// <returns>New Color</returns>
         public static TCODColor Interpolate(TCODColor c1, TCODColor c2, double coef)
         {
         	TCODColor ret =  new TCODColor(); 
