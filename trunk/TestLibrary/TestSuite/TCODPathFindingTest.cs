8a3d1576dd11374251b54e4c22f28f987a352470
chris.hamons <chris.hamons@2fc39768-4c50-0410-98c4-473688cc0402>
2008-07-26 00:44:34 +0000

    Add libtcod's path finding support (from svn currently)

---
+++ trunk/TestLibrary/TestSuite/TCODPathFindingTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using libtcodWrapper;
+
+namespace libtcodWrapperTests
+{
+    [TestFixture]
+    public class TCODPathFindingTest
+    {
+        static char[,] room =
+            {
+                {'#', '#', '#', '#', '#'},
+                {'#', '.', '#', '.', '#'},
+                {'#', '.', '#', '.', '#'},
+                {'#', '.', '.', '.', '#'},
+                {'#', '#', '#', '#', '#'}
+            };
+
+        public struct point
+        {
+            public point(int x, int y)
+            {
+                m_x = x;
+                m_y = y;
+            }
+            public int m_x;
+            public int m_y;
+        }
+        static point[] walkList = {new point(1, 2), new point(2, 3), new point(3, 3) };
+
+        static bool PathCallback(int x, int y, ref Object userData)
+        {
+            return (room[x,y] == '.');
+        }
+        
+        private TCODPathFinding pathFindingFOV;
+        private TCODFov fov;
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            fov = new TCODFov(5, 5);
+
+            for (int i = 0; i < 5; ++i)    //width
+                for (int j = 0; j < 5; ++j) //height
+                    fov.SetCell(i, j, room[j, i] == '.', room[j, i] == '.');
+
+            pathFindingFOV = new TCODPathFinding(fov);
+        }
+
+        [TestFixtureTearDown]
+        public void Cleaup()
+        {
+            pathFindingFOV.Dispose();
+            fov.Dispose();
+        }
+
+        [Test]
+        public void ComputeSucessful()
+        {
+            Assert.IsTrue(pathFindingFOV.ComputePath(1, 1, 3, 3));
+        }
+
+        [Test]
+        public void ComputImpossiblePath()
+        {
+            Assert.IsFalse(pathFindingFOV.ComputePath(1, 1, 4, 4));
+        }
+
+        [Test]
+        public void WalkExpectedPath()
+        {
+            ComputeSucessful();
+            int x = 1;
+            int y = 1;
+            int i = 0;
+            while (pathFindingFOV.WalkPath(ref x, ref y, true))
+            {
+                Assert.IsTrue(x == walkList[i].m_x);
+                Assert.IsTrue(y == walkList[i].m_y);
+                i++;
+            }
+        }
+
+        [Test]
+        public void CheckIndividualPoints()
+        {
+            int index = 1;
+            int x;
+            int y;
+            ComputeSucessful();
+            pathFindingFOV.GetPointOnPath(index, out x, out y);
+            Assert.IsTrue(x == 2 && y == 3);
+        }
+
+        [Test]
+        public void CheckPathEmpty()
+        {
+            ComputImpossiblePath();
+            Assert.IsTrue(pathFindingFOV.IsPathEmpty());
+            ComputeSucessful();
+            Assert.IsFalse(pathFindingFOV.IsPathEmpty());
+        }
+
+        [Test]
+        public void CheckPathSize()
+        {
+            ComputImpossiblePath();
+            Assert.IsTrue(pathFindingFOV.GetPathSize() == 0);
+            ComputeSucessful();
+            Assert.IsTrue(pathFindingFOV.GetPathSize() == 3);
+        }
+    }
+}
---
+++ trunk/libtcodWrapper/TCODPathFinding.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace libtcodWrapper
+{
+    /// <summary>
+    /// Calculates paths in maps using djikstra's algorithms
+    /// </summary>
+    public class TCODPathFinding : IDisposable
+    {
+        private IntPtr m_instance;
+
+        /// <summary>
+        /// Create new TCODPathFinding using map from TCODFov instance
+        /// </summary>
+        /// <param name="fovMap">Existing map</param>
+        public TCODPathFinding(TCODFov fovMap)
+        {
+            m_instance = TCOD_path_new_using_map(fovMap.m_mapPtr);
+        }
+
+        /// <summary>
+        /// Compute a path from source to destination. 
+        /// </summary>
+        /// <param name="origX">Starting point x coord</param>
+        /// <param name="origY">Starting point y coord</param>
+        /// <param name="destX">Destination point x coord</param>
+        /// <param name="destY">Destination point y coord</param>
+        /// <returns>IsPathFound?</returns>
+        public bool ComputePath(int origX, int origY, int destX, int destY)
+        {
+            return TCOD_path_compute(m_instance, origX, origY, destX, destY);
+        }
+
+        /// <summary>
+        /// Walk along a path. Fill x and y with previous step's coord to get next point.
+        /// </summary>
+        /// <param name="x">x coord</param>
+        /// <param name="y">y coord</param>
+        /// <param name="recalculateWhenNeeded">If path comes to abrupt end, can we spend time looking for route?</param>
+        /// <returns>MoreToWalkAlong?</returns>
+        public bool WalkPath(ref int x, ref int y, bool recalculateWhenNeeded)
+        {
+            return TCOD_path_walk(m_instance, ref x, ref y, recalculateWhenNeeded);
+        }
+
+        /// <summary>
+        /// Query individual point on path
+        /// </summary>
+        /// <param name="index">0-based index of points in path list</param>
+        /// <param name="x">x coord of point</param>
+        /// <param name="y">y coord of point</param>
+        public void GetPointOnPath(int index, out int x, out int y)
+        {
+            TCOD_path_get(m_instance, index, out x, out y);
+        }
+
+        /// <summary>
+        /// Returns if path is empty of points
+        /// </summary>
+        /// <returns>IsEmpty?</returns>
+        public bool IsPathEmpty()
+        {
+            return TCOD_path_is_empty(m_instance);
+        }
+
+        /// <summary>
+        /// Get remainding points on path
+        /// </summary>
+        /// <returns>Path Size</returns>
+        public int GetPathSize()
+        {
+            return TCOD_path_size(m_instance);
+        }
+
+        /// <summary>
+        /// Destory unmanaged pathfinding resource.
+        /// </summary>
+        public void Dispose()
+        {
+            TCOD_path_delete(m_instance);
+        }
+
+        #region DllImport
+        [DllImport(DLLName.name)]
+        private extern static IntPtr TCOD_path_new_using_map(IntPtr map);
+
+        [DllImport(DLLName.name)]
+        private extern static bool TCOD_path_compute(IntPtr path, int origX, int origY, int destX, int destY);
+
+        [DllImport(DLLName.name)]
+        private extern static bool TCOD_path_walk(IntPtr path, ref int x, ref int y, bool recalculate_when_needed);
+
+        [DllImport(DLLName.name)]
+        private extern static bool TCOD_path_is_empty(IntPtr path);
+
+        [DllImport(DLLName.name)]
+        private extern static int TCOD_path_size(IntPtr path);
+
+        [DllImport(DLLName.name)]
+        private extern static void TCOD_path_get(IntPtr path, int index, out int x, out int y);
+        
+        [DllImport(DLLName.name)]
+        private extern static void TCOD_path_delete(IntPtr path);
+        #endregion
+
+    }
+}
